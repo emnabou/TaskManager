f921f08756b45f7415ac8fec776e204720cdb042
emnabou <emna_bouzayeni@hotmail.com>
2025-03-12 13:08:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,37 +10,7 @@
 
 int main()
 {
-    /*std::ifstream taskfile("task.txt");
-    if(!taskfile.is_open())
-    {
-        std::cout << "Error: file not found" << std::endl;
-        return 1;
-    }
-
-    std::string line;
-
-    while(getline(taskfile,line))
-    {
-        std::cout<<line<<std::endl;
-    }
-
-
-
-    taskfile.close();*/
-    //TaskManager manager("task.txt");
-    //manager.run();
     TaskManager manager("task.txt");
-    /*manager.Addtask("Go to the gym",1);
-    manager.Addtask("Buy groceries",2);
-    manager.Addtask("Do homework",3);
-    manager.ListTasks();
-    manager.UpdateTask(1,true);
-    manager.ListTasks();
-    manager.DeleteTask(2);*/
-    //manager.ListTasks();
-    //manager.run();
-    manager.getnextId();
-    std::cout<<manager.getnextId()<<std::endl;
     manager.run();
     return 0;
 }
